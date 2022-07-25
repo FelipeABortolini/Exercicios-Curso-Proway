e15ff02dfe3348f6b7b0d07af4c7946f04d97207
Felipe Augusto Bortolini <felipebortolini2001@gmail.com>
2022-07-25 15:02:47 -0300

    Delete Dart/Avançado directory

--- Dart/Avançado/dart-advanced/classes/injecao-dependencia/inherited_widget/lib/core/app_assets.dart
+++
@@ -1,3 +0,0 @@
-const String program = "assets/image/program.png";
-const String server = "assets/image/server.png";
-const String weather = "assets/image/weather.png";
--- Dart/Avançado/dart-advanced/generics/exercise_generics.dart
+++
@@ -1,2 +0,0 @@
-// Faça uma classe genérica, que espera um atributo genérico, peça no construtor.
-// e tenha um método que imprima o valor desse atributo e retorne esse valor, usando generics.
