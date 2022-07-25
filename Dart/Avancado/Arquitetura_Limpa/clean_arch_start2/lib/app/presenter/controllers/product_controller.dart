6c0746d649be0d4715eacf58f224b29a6cb316de
Felipe Augusto Bortolini <Felipe.Bortolini@proway.treina>
2022-07-25 14:57:20 -0300

    cdfce

--- Dart/Avancado/Arquitetura_Limpa/clean_arch_start2/lib/app/presenter/controllers/product_controller.dart
+++ Dart/Avancado/Arquitetura_Limpa/clean_arch_start2/lib/app/presenter/controllers/product_controller.dart
@@ -2,11 +2,17 @@ import '../../domain/entities/product_entity.dart';
 import '../../domain/usecases/get_all_products_usecase.dart';
 
 class ProductController {
-  GetAllProductsUsecase _getAllProductsUsecase;
+  // No nosso controller nós usamos os UseCase
+  // Por isso nós pedimos uma isntânciado CONTRATO
+  // no construtor, fazendo o DIP.
+  final GetAllProductsUsecase _getAllProductsUsecase;
   ProductController(this._getAllProductsUsecase) {
+    // Aqui no construtor fazemos a chamada do método getAllProducts
+    // para trazer os produtos.
     getAllProducts();
   }
 
+  // Criamos um objeto aqui para usar ele nas pages.
   List<ProductEntity> products = [];
   void getAllProducts() {
     products = _getAllProductsUsecase.call();
